0667cf801d3e4291abf860303e67658000cbd648
Hitesh Patel <hbp976@gmail.com>
2020-09-17 14:03:08 -0600

    Hello Tech component added in branch hello-tech

---
+++ src/component/HelloTech.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HelloTech () {
+    return (
+        <h1>Hello TECHCareer Student</h1>
+
+    );
+}
+
+export default HelloTech;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloTech from './component/HelloTech';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <HelloTech />
   </React.StrictMode>,
   document.getElementById('root')
 );
